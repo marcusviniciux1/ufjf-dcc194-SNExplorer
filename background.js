4a69c3a6015522611245b89e06aae73e6bc7fd4f
Vinicius <marcusvinicius.rodrigues@estudante.ufjf.br>
2025-01-11 13:30:42 -0300

    Adicionar menu de contexto

--- background.js
+++ background.js
@@ -1,3 +1,27 @@
 chrome.runtime.onInstalled.addListener(() => {
   console.log("SN Explorer instalado com sucesso!");
+
+  chrome.contextMenus.create({
+    id: "pasteTest",
+    title: "Colar texto: teste",
+    contexts: ["editable"],
+  });
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === "pasteTest") {
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      func: () => {
+        const activeElement = document.activeElement;
+        if (
+          activeElement &&
+          (activeElement.tagName === "TEXTAREA" ||
+            activeElement.tagName === "INPUT")
+        ) {
+          activeElement.value += "teste";
+        }
+      },
+    });
+  }
 });
